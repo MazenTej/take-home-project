910c67a7e41500d35b705bc34c4a0bd306708f6b
`MazenTej` <mtej.yestunisia2016@gmail.com>
2021-06-15 21:36:32 -0400

    finished work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { mockFetchHelper } from "./mock_api";
+import { useState } from "react";
+import albums from "./albums.json";
+import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Button } from "@material-ui/core";
 
 function App() {
+  var show = false;
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+  const classes = useStyles();
+
+  const [data, setData] = useState();
+
+  function handleClick() {
+    const promise = mockFetchHelper(true, data, 1000);
+    promise.then(handleCall);
+  }
+
+  function handleCall() {
+    setData(albums.albums);
+  }
+  if (data != null) {
+    show = true;
+  }
+
+  if (show) {
+    for (var i = 0; i <= 4; i++) {
+      delete data[i].avg_user_rating;
+      data[i].release_date = formatDate(data[i].release_date);
+      data[i].genres = data[i].genres.toString();
+
+      for (var j = 1; j <= 4; j++) {
+        if (data[j].last_listened > data[i].last_listened) {
+          const b = data[i];
+          data[i] = data[j];
+          data[j] = b;
+        }
+      }
+    }
+  }
+
+  function formatDate(date) {
+    return moment(date).format("MM/DD/YYYY HH:MM");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {show === false && (
+        <div className="container">
+          <div className="center">
+            <Button
+              className="button"
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+            >
+              Click me
+            </Button>
+          </div>
+        </div>
+      )}
+      <TableContainer component={Paper}>
+        {show === true && (
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Album</TableCell>
+                <TableCell>Band</TableCell>
+                <TableCell>Genres</TableCell>
+                <TableCell>Last Played</TableCell>
+                <TableCell>Release Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow key={row.album_title}>
+                  <TableCell>{row.album_title}</TableCell>
+                  <TableCell>{row.band_name}</TableCell>
+                  <TableCell>{row.genres}</TableCell>
+                  <TableCell>{row.last_listened}</TableCell>
+                  <TableCell>{row.release_date}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </TableContainer>
     </div>
   );
 }
